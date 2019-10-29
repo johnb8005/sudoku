25448060f2811904bf091e3183e7f496a1fbea60
John B <john@nexys.ch>
2019-10-29 20:25:15 +0100

    solves all reasonable easy sudokus

--- src/sudoku.js
+++ src/sudoku.js
@@ -31,14 +31,14 @@ export const initSolset = (sudoku, dim = 3) => {
  * @param  i: row index (starts at 1)
  * @return e.g. [1, 2, 3, ...]
  */
-export const getRowCoords = (i, dim = 3) => Array(dim * dim).fill(0).map((x, k) =>  (i - 1)*dim*dim + k);
+export const getRowCoords = (i, dim = 3) => Array(dim * dim).fill(0).map((x, k) =>  i*dim*dim + k);
 
 /**
  * get the list of coordinates for a column
  * @param  i: row index (starts at 1)
  * @return e.g. [1, 9, 18, ...]
  */
-export const getColCoords = (i, dim = 3) => Array(dim * dim).fill(0).map((x, k) =>  (i - 1) + dim*dim*k);
+export const getColCoords = (i, dim = 3) => Array(dim * dim).fill(0).map((x, k) =>  i + dim*dim*k);
 
 /**
  * get the list of coordinates for a cell of cells (in regular sudoku, 3x3 cells)
@@ -46,12 +46,27 @@ export const getColCoords = (i, dim = 3) => Array(dim * dim).fill(0).map((x, k) 
  * @return e.g. [1, 2, 3, 9, 10, 11, ...]
  */
 export const getCellCoords = (i, dim = 3) => {
+  if (dim === 3) {
+    if (i=== 0) return [0, 1, 2,  9, 10, 11, 18, 19, 20];
+    if (i=== 1) return [3, 4, 5, 12, 13, 14, 21, 22, 23];
+    if (i=== 2) return [6, 7, 8, 15, 16, 17, 24, 25, 26];
+  // second three cells (from left to right)
+    if (i=== 3) return [27, 28, 29, 36, 37, 38, 45, 46, 47];
+    if (i=== 4) return [30, 31, 32, 39, 40, 41, 48, 49, 50];
+    if (i=== 5) return [33, 34, 35, 42, 43, 44, 51, 52, 53];
+  // third three cells (from left to right)
+    if (i=== 6) return [54, 55, 56, 63, 64, 65, 72, 73, 74];
+    if (i=== 7) return [57, 58, 59, 66, 67, 68, 75, 76, 77];
+    if (i=== 8) return [60, 61, 62, 69, 70, 71, 78, 79, 80];
+  }
+
+
   const r = [];
 
   // first adder
-  const a1 = ((i-1) % dim )*dim;
+  const a1 = (i % dim )*dim;
   // second adder
-  const a2 = Math.floor((i-1)/2)*dim*dim*dim;
+  const a2 = Math.floor(i/2)*dim*dim*dim;
 
   for (let j = 0; j < dim; j++) {
     for (let k = 0; k < dim; k++) {
@@ -63,6 +78,24 @@ export const getCellCoords = (i, dim = 3) => {
   return r;
 }
 
+export const list2Possibilities = (row, coords) => {
+  // filter out all the cells that have 2 digits as a possibility
+  const twoPossibilities = row.filter((r, i) => Array.isArray(r) && r.length === 2 && coords.includes(i));
+
+  // keep the duplicates
+  // go through the upper right triangle (avoid extra unnecessary computation) matrix (formed by the two vectors)
+  return twoPossibilities.flatMap((x, i) => {
+    return twoPossibilities.map((y, j) => {
+      if (i > j && compareArray(x, y)) {
+        return x;
+      }
+
+      return null;
+    });
+  })
+  .filter(_ => _ !== null);
+}
+
 /**
  * looks at one "set". a set here is defined as an array where all elements should uniquely contain all digits. In the normal sudoku, that includes rows, cells and columns
  * @param row: full array of sudoku
@@ -72,19 +105,39 @@ export const getCellCoords = (i, dim = 3) => {
 export const checkRow = (row, coords) => {
   const temps = row.filter((r, i) => typeof r === 'number' && coords.includes(i));
 
+  const listof2 = list2Possibilities(row, coords)
+
   return row.map((r, i) => {
+    // check if it is a single digit
     if(typeof r === 'number' || !coords.includes(i)) {
       return r;
     }
 
+    // when an array substract the other ruled out possibilities (without substrating itself)
     const x = substractArray(r, temps);
+    const y = substractArrayFromArray(x, listof2);
 
-    if (x.length === 1) {
-      return x[0];
+    if (y.length === 1) {
+      return y[0];
     }
 
-    return x;
+    return y;
   });
+}
+
+/**
+ * compares two unsorted array 
+ * @param  {[type]} a [description]
+ * @param  {[type]} b [description]
+ * @return true if same array
+ */
+export const compareArray = (a, b) => {
+  if ( a.length != b.length) {
+    return false;
+  }
+  return a.filter(function(i) {
+    return !b.includes(i);
+  }).length === 0;  
 }
 
 /**
@@ -92,17 +145,25 @@ export const checkRow = (row, coords) => {
  */
 export const substractArray = (a, s) => a.filter(x => !s.includes(x));
 
+export const substractArrayFromArray = (a, ss) => {
+  if (a.length === 2 && !compareArray(a, ss)) {
+    return a;
+  }
+
+  return substractArray(a, ss.flat());
+}
+
 /**
  * generalization of checkRow
  * @param  {[type]} funcCoord : the function that describes the set
 
  */
 export const checkDim = (sudoku, funcCoord, dim, rowIdx = 0) => {
-  if (rowIdx === dim * dim) {
+  if (rowIdx === dim * dim ) {
     return sudoku;
   }
 
-  const coords = funcCoord(rowIdx + 1, dim);
+  const coords = funcCoord(rowIdx, dim);
   const s = checkRow(sudoku, coords);
 
   return checkDim(s, funcCoord, dim, rowIdx + 1);
@@ -127,7 +188,7 @@ export const solveIteration = (sudoku, dim = 3, strategies = [getRowCoords, getC
  * iterates over `solveIteration`
  */
 export const solve = (sudoku, dim = 3, nEpochs = 30) => {
-  console.log(`=== computing epoch ${nEpochs} (counting backwards) ===`)
+  //console.log(`=== computing epoch ${nEpochs} (counting backwards) ===`)
   if (nEpochs === 0) {
     console.log(`algorithm after all epochs were exhausted (${nEpochs})`);
     return sudoku;
@@ -135,7 +196,7 @@ export const solve = (sudoku, dim = 3, nEpochs = 30) => {
 
   const nFound = sudoku.filter(x => typeof x !== 'number').length;
 
-  console.log(`the grid contains ${nFound}/${dim ** 4} cells that are undefined (${(100 * nFound/(dim ** 4)).toFixed(2)}%)`)
+  //console.log(`the grid contains ${nFound}/${dim ** 4} cells that are undefined (${(100 * nFound/(dim ** 4)).toFixed(2)}%)`)
 
   // check if something is left to solve, else return array
   if(nFound === 0) {
@@ -152,3 +213,12 @@ export const solveWithInit = (sudoku, dim = 3, nIteration = 30) => {
   const s0 = initSolset(sudoku);
   return solve(s0, dim, nIteration);
 }
+
+
+export const printSudoku = (s, dim = 3) => Array(dim**2).fill(0).map((_, j) => {
+  return s
+    .filter((x, i) => i >= j*9 && i <= (j+1)*9 -1)
+    .map(x => x.length > 2 ? null : x )
+    .reduce((a, b) => `${a}\t${b}`);
+})
+.reduce((a, b) => `${a}\n${b}`);
--- src/sudoku.test.js
+++ src/sudoku.test.js
@@ -8,6 +8,8 @@ test('subtract array', () => {
   expect(Sudoku.substractArray(a, s)).toEqual(e)
 });
 
+
+
 //
 const dim = 2;
 
@@ -32,38 +34,63 @@ test('check row', () => {
   const row3 = [[1, 2, 3, 4], 2, [1, 2, 3, 4], [1, 2, 3, 4]]; 
   const e3 = [[1, 3, 4],  2, [1, 3, 4], [1, 3,4]];
   expect(Sudoku.checkRow(row3, coords)).toEqual(e3);
+
+
+  const row4 = [[1, 3], 2, [1, 3], [1, 2, 3, 4]]; 
+  const e4 = [[1, 3], 2, [1, 3], 4];
+  expect(Sudoku.checkRow(row4, coords)).toEqual(e4);
+  
+  
+  // have test for of list 2 possibilities (see test below)
+  const row5 = [[1, 3, 2], [1, 2], [3, 7], [3, 7], [3, 7, 1], 6, [8,9], [9,8], [1, 3, 2]];
+  const e5   = [[1, 2],    [1, 2], [3, 7], [3, 7], 1,        6, [8, 9], [9,8], [1, 2]];
+  const coords5 = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  const r5 = Sudoku.checkRow(row5, coords5);
+  expect(r5).toEqual(e5);
 });
+
 
 test('get row coords', () => {
   // 2 dim
-  expect(Sudoku.getRowCoords(1, dim)).toEqual([0, 1, 2, 3]);
-  expect(Sudoku.getRowCoords(2, dim)).toEqual([4,5, 6, 7]);
+  expect(Sudoku.getRowCoords(0, dim)).toEqual([0, 1, 2, 3]);
+  expect(Sudoku.getRowCoords(1, dim)).toEqual([4, 5, 6, 7]);
   // 3 dim
-  expect(Sudoku.getRowCoords(1)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
-  expect(Sudoku.getRowCoords(2)).toEqual([9, 10, 11, 12, 13, 14, 15, 16, 17]);
-})
+  expect(Sudoku.getRowCoords(0)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8]);
+  expect(Sudoku.getRowCoords(1)).toEqual([9, 10, 11, 12, 13, 14, 15, 16, 17]);
+});
 
 test('get col coords', () => {
   // 2 dim
-  expect(Sudoku.getColCoords(1, dim)).toEqual([0, 4, 8, 12]);
-  expect(Sudoku.getColCoords(2, dim)).toEqual([1, 5, 9, 13]);
+  expect(Sudoku.getColCoords(0, dim)).toEqual([0, 4, 8, 12]);
+  expect(Sudoku.getColCoords(1, dim)).toEqual([1, 5, 9, 13]);
   // 3 dim
-  expect(Sudoku.getColCoords(1)).toEqual([0, 9, 18, 27, 36, 45, 54, 63, 72]);
-  expect(Sudoku.getColCoords(2)).toEqual([1, 10, 19, 28, 37, 46, 55, 64, 73]);
+  expect(Sudoku.getColCoords(0)).toEqual([0, 9, 18, 27, 36, 45, 54, 63, 72]);
+  expect(Sudoku.getColCoords(1)).toEqual([1, 10, 19, 28, 37, 46, 55, 64, 73]);
 })
 
 test('get cell coords', () => {
   // 2 dim
-  expect(Sudoku.getCellCoords(1, dim)).toEqual([0, 1, 4, 5]);
-  expect(Sudoku.getCellCoords(2, dim)).toEqual([2, 3, 6, 7]);
-  expect(Sudoku.getCellCoords(3, dim)).toEqual([8, 9, 12, 13]);
-  expect(Sudoku.getCellCoords(4, dim)).toEqual([10, 11, 14, 15]);
+  expect(Sudoku.getCellCoords(0, dim)).toEqual([0, 1, 4, 5]);
+  expect(Sudoku.getCellCoords(1, dim)).toEqual([2, 3, 6, 7]);
+  expect(Sudoku.getCellCoords(2, dim)).toEqual([8, 9, 12, 13]);
+  expect(Sudoku.getCellCoords(3, dim)).toEqual([10, 11, 14, 15]);
   // 3 dim
-  expect(Sudoku.getCellCoords(1)).toEqual([0, 1, 2, 9, 10, 11, 18, 19, 20]);
-  expect(Sudoku.getCellCoords(4)).toEqual([27, 28, 29, 36, 37, 38, 45, 46, 47]);
+  // first three cells (from left to right)
+  expect(Sudoku.getCellCoords(0)).toEqual([0, 1, 2, 9, 10, 11, 18, 19, 20]);
+  expect(Sudoku.getCellCoords(1)).toEqual([3, 4, 5, 12, 13, 14, 21, 22, 23]);
+  expect(Sudoku.getCellCoords(2)).toEqual([6, 7, 8, 15, 16, 17, 24, 25, 26]);
+  // second three cells (from left to right)
+  expect(Sudoku.getCellCoords(3)).toEqual([27, 28, 29, 36, 37, 38, 45, 46, 47]);
+  expect(Sudoku.getCellCoords(4)).toEqual([30, 31, 32, 39, 40, 41, 48, 49, 50]);
+  expect(Sudoku.getCellCoords(5)).toEqual([33, 34, 35, 42, 43, 44, 51, 52, 53]);
+  // third three cells (from left to right)
+  expect(Sudoku.getCellCoords(6)).toEqual([54, 55, 56, 63, 64, 65, 72, 73, 74]);
+  expect(Sudoku.getCellCoords(7)).toEqual([57, 58, 59, 66, 67, 68, 75, 76, 77]);
+  expect(Sudoku.getCellCoords(8)).toEqual([60, 61, 62, 69, 70, 71, 78, 79, 80]);
 });
 
-test('check rows', () => {
+
+test('checkdim', () => {
   const s1 = [
                1, [1, 2, 3, 4], 3,           4,
     [1, 2, 3, 4], 3,            2,           1,
@@ -79,9 +106,10 @@ test('check rows', () => {
   ];
 
   const s = Sudoku.checkDim(s1, Sudoku.getRowCoords, dim);
-
   expect(s).toEqual(e1);
 });
+
+
 
 test('check cols', () => {
   const s1 = [
@@ -123,129 +151,77 @@ test('check cells', () => {
   expect(s).toEqual(e1);
 });
 
-test('real life test (2dim)', () => {
-  const s = [
-    1,    null, 3,    4,
-    4,    null, null, 1,
-    2,    1,    4,    null,
-    null, null, 1,    2
-  ];
 
-  const s0 = Sudoku.initSolset(s, 2);
-  const t = Sudoku.solveIteration(s0, 2);
+test('list2Possibilities', () => {
+  const rows = [[3, 7], [7,3], [1, 3, 7]];
+  const coords = [0, 1, 2];
 
-  expect(t).toEqual([
-    1, 2, 3, 4,
-    4, 3, 2, 1,
-    2, 1, 4, 3,
-    3, 4, 1, 2])
+  const r = Sudoku.list2Possibilities(rows, coords);
+  const e = [[7, 3]];
+
+  expect(r).toEqual(e);
 });
 
-test('real life test (3dim)', () => {
-  /*const s2 = [
-    3, 4, 9, 6, null, 2, null, null, null,
-    null,  1, 8, 7, null, null, 2, null, null,
-    null, 7, 2, null, 9, null, null, null, 8,
-    null, null, null, null, 7, 4, null, null, 9,
-    null, null, null, null, 2, null, 3, null, 5,
-    null, null, null, 1, 6, null, null, 4, null,
-    null, 2, null, null, 1, null, 9, 5, null,
-    null,5, null, 2, null, 6, null, null, null,
-    7, null, null, 3, null, 9, 4, 1, null
-  ];*/
+test('list2Possibilities (2)', () => {
+  const rows = [[3, 7], [3 ,7], [1, 3, 7]];
+  const coords = [0, 1, 2];
 
-  const s = [
-    null, null, null, 4, null, null, null, 8, null,
-    null, null, 6, null, 8, null, 1, null, 3,
-    null, 8, null, 1, null, 3, null, 5, null,
-    2, null, null, null, 6, null, 8, null, 1,
-    null, 6, null, 8, null, 1, null, 3, 4,
-    null, null, 1, null, 3, 4, null, 6, null,
-    null, 4, null, 6, 7, null, 9, null, 2,
-    6, null, null, 9, null, 2, null, 4, 5,
-    null, null, 2, null, null, null, null, null, null
-  ];
+  const r = Sudoku.list2Possibilities(rows, coords);
+  const e = [[3, 7]];
 
-  const e = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9,
-    4, 5, 6, 7, 8, 9, 1, 2, 3,
-    7, 8, 9, 1, 2, 3, 4, 5, 6,
-    2, 3, 4, 5, 6, 7, 8, 9, 1,
-    5, 6, 7, 8, 9, 1, 2, 3, 4,
-    8, 9, 1, 2, 3, 4, 5, 6, 7,
-    3, 4, 5, 6, 7, 8, 9, 1, 2,
-    6, 7, 8, 9, 1, 2, 3, 4, 5,
-    9, 1, 2, 3, 4, 5, 6, 7, 8
-  ];
-
-  const t = Sudoku.solveWithInit(s);
-
-  expect(t).toEqual(e);
+  expect(r).toEqual(e);
 });
 
-test('example from previous app', () => {
-  const arr = Array(81).fill(null)
-  arr[0+0] = 9;
-  arr[2+0] = 3;
-  arr[3+0] = 7;
-  arr[4+0] = 1;
-  arr[7+0] = 8;
-  arr[3+9] = 6;
-  arr[4+9] = 9;
-  arr[6+9] = 7;
-  arr[7+9] = 3;
-  arr[8+9] = 2;
-  arr[0+18] = 2;
-  arr[1+18] = 7;
-  arr[2+18] = 6;
-  arr[3+18] = 3;
-  arr[6+18] = 1;
+test('list2Possibilities (3)', () => {
+  const rows = [[3, 7], [1, 3, 7]];
+  const coords = [0, 1, 2];
 
-  arr[1+27] = 6;
-  arr[5+27] = 9;
-  arr[6+27] = 2;
-  arr[7+27] = 7;
-  arr[8+27] = 5;
-  arr[0+36] = 3;
-  arr[2+36] = 4;
-  arr[3+36] = 2;
-  arr[5+36] = 7;
-  arr[8+36] = 6;
-  arr[1+45] = 2;
-  arr[2+45] = 7;
-  arr[4+45] = 6;
-  arr[5+45] = 1;
-  arr[7+45] = 9;
+  const r = Sudoku.list2Possibilities(rows, coords);
+  const e = [];
 
-  arr[0+54] = 1;
-  arr[1+54] = 8;
-  arr[2+54] = 5;
-  arr[4+54] = 2;
-  arr[7+54] = 4;
-  arr[3+63] = 5;
-  arr[5+63] = 3;
-  arr[6+63] = 9;
-  arr[7+63] = 6;
-  arr[8+63] = 1;
-  arr[0+72] = 6;
-  arr[1+72] = 3;
-  arr[4+72] = 7;
-  arr[5+72] = 4;
-  arr[6+72] = 5;
+  expect(r).toEqual(e);
+});
 
-  const t = Sudoku.solveWithInit(arr);
+test('list2Possibilities (4)', () => {
+  const rows = [[3, 7], [3 ,7], [2, 4], [2, 4], [1, 3, 7]];
+  const coords = [0, 1, 2, 3, 4];
 
-  const e = [
-    9, 5, 3, 7, 1, 2, 6, 8, 4,
-    4, 1, 8, 6, 9, 5, 7, 3, 2,
-    2, 7, 6, 3, 4, 8, 1, 5, 9,
-    8, 6, 1, 4, 3, 9, 2, 7, 5,
-    3, 9, 4, 2, 5, 7, 8, 1, 6,
-    5, 2, 7, 8, 6, 1, 4, 9, 3,
-    1, 8, 5, 9, 2, 6, 3, 4, 7,
-    7, 4, 2, 5, 8, 3, 9, 6, 1,
-    6, 3, 9, 1, 7, 4, 5, 2, 8
-  ];
+  const r = Sudoku.list2Possibilities(rows, coords);
+  const e = [[3, 7], [2, 4]];
 
-  expect(t).toEqual(e);
+  expect(r).toEqual(e);
+});
+
+test('list2Possibilities (4)', () => {
+  const row = [[1, 3, 2], [1, 2], [3, 7], [3, 7], [3, 7, 1], 6, [8,9], [9,8], [1, 3, 2]];
+  const coords = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  const r = Sudoku.list2Possibilities(row, coords);
+  const e   = [ [ 3, 7 ], [ 9, 8 ] ];
+
+  expect(r).toEqual(e);
+});
+
+test('substractArrayFromArray', () => {
+
+  
+  const s = [[3, 7], [9,8]];
+  const a = [1, 3, 7];
+  
+  const e = [1];
+  const r = Sudoku.substractArrayFromArray(a, s);
+
+  expect(r).toEqual(e);
 })
+
+test('substractArrayFromArray 2', () => {
+  //const row = [[1, 3, 2], [1, 2], [3, 7], [3, 7], [3, 7, 1], 6, [8,9], [9,8], [1, 3, 2]];
+  // result of list 2 possibilities (see test below)
+  const s = [[3, 7], [9,8]];
+  const a = [ 3, 7];
+  //const e   = [[1, 2],    [1, 2], [3, 7], [3, 7], 1,       , 6, [8, 9], [9,8], [1,2]];
+  const e = [3, 7];
+  const r = Sudoku.substractArrayFromArray(a, s);
+
+  expect(r).toEqual(e);
+});
