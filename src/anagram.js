895246221e7f57fdf3b59075d3fc9de8e480a8af
John B <john@nexys.ch>
2019-11-11 17:44:20 +0100

    added UI

--- src/anagram.js
+++ src/anagram.js
@@ -1,30 +1,5 @@
-export const isAnagram = (w1, w2) => {
-  const w1arr = w1.split('');
-  const w2arr = w2.split('');
+import React from 'react';
 
-  if (w2arr.length != w1arr.length) {
-    return false;
-  }
+const P = () => <p><i>Not implemented yet</i></p>
 
-  return w1arr.map(c1 => {
-    const wi = w2arr.indexOf(c1);
-
-    if (wi > -1) {
-      delete(w2arr[wi]);
-      return true;
-    }
-
-    return false;
-  })
-  .reduce((a, b) => a && b);
-}
-
-export const findAnagram = (w, dict) => dict.filter(word => isAnagram(w, word));
-
-export const findAndPrintAnagram = (w, dict) => {
-  const anagrams = findAnagram(w, dict);
-
-  anagrams.map(w => {
-    console.log(`${word} is an anagram of ${w}`);
-  });
-}
+export default P;
